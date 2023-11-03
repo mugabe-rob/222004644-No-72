8ac2d7ea02ec709382a123b56a91eba4d90f169a
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 10:45:08 +0200

    A java program showing the right shift operator.

---
+++ rightshiftOperator.java
@@ -0,0 +1,10 @@
+public class rightShiftoperator {
+    public static void main(String args[]){
+ 
+        //RIGHT SHIFT OPERATOR
+System.out.println(10>>2);//2  
+System.out.println(20>>2);//5  
+System.out.println(20>>3);//2 
+
+}
+}
