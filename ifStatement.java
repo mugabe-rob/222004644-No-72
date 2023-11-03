a8c37d20359fa50acef6bed4a622d34c9c63ece9
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 11:05:29 +0200

    A program to show display traffic lights using if else ststements.

---
+++ ifStatement.java
@@ -0,0 +1,22 @@
+public class age {
+    public static void main(String a[]) {
+        String Lights = "Red";
+
+        if (Lights == "Red") 
+        {
+            System.out.println("Stop");
+        }
+         else if (Lights == "Yellow") 
+         {
+            System.out.println("Slow down");
+        } 
+        else if (Lights == "Green") 
+        {
+            System.out.println("Go");
+        } 
+        else 
+        {
+            System.out.println("Invalid light color");
+        }
+    }
+}
