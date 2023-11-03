f5db32418d9a06740808c7dd7a4d3238a786fafc
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 19:09:38 +0200

    Aprogram to display numbers from 1 to 10 using a While loop.

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1; // Initializing a counter variable
+        
+        while (i <= 10) { // While the counter is less than or equal to 10
+            System.out.println("Count: " + i); // Print the current value of the counter
+            i++; // Increment the counter by 1
+        }
+    }
+}
