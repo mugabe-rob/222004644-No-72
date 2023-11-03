08c3fa2a2dc09b2948ffe097fadb8b61ca208233
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 11:20:34 +0200

    A program to print an inverted triangle using for loop

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+public class forLoop
+{
+public static void main(String args[])
+{
+ int no=10;
+        for(int i=1;i<=no;i++){
+            for(int b=no;b>=i;b--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
