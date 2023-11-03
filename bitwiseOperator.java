184fbf53de38acb025a43ed66bd529a402f78338
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 11:02:02 +0200

    A program showing the bitwise Operator.

---
+++ bitwiseOperator.java
@@ -0,0 +1,14 @@
+public class bitwiseOperator {
+public static void main(String args[]){
+int x=10;
+int y=-10;
+boolean c=true;
+boolean d=false;
+
+
+System.out.println(~x);//-11
+System.out.println(~y);//9
+System.out.println(!c);//false
+System.out.println(!d);//True
+}
+}
