c01a062e900b734355ca0a13b222ba8de6649b0a
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 10:47:55 +0200

    A program showing the Left shift operator.

---
+++ LeftshiftOperator.java
@@ -0,0 +1,10 @@
+public class LeftshiftOperator {
+public static void main(String args[])
+{
+ //Left shift operator
+System.out.println(10<<2);//40
+System.out.println(10<<3);//80
+System.out.println(20<<2);//80
+System.out.println(15<<4);//240
+}
+}
