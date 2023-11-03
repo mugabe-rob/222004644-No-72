7b1482ef028d38f045acde77c265211be951390a
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 10:51:21 +0200

    A program showing how arithmetic operators work.

---
+++ ArithmeticOperators.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperators {
+public static void main(String args[]){
+int ac=10;
+int b=5;
+
+System.out.println(ac+b);//15
+System.out.println(ac-b);//5
+System.out.println(ac*b);//50
+System.out.println(ac/b);//2
+System.out.println(ac%b);//0
+
+}
+}
