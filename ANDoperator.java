fa6c9132e61cd3b56ca52eefe73da4c979dba93a
MUGABE Robert <139157186+mugabe-rob@users.noreply.github.com>
2023-11-03 10:41:09 +0200

    How AND operator works

---
+++ ANDoperator.java
@@ -0,0 +1,12 @@
+public class UnaryOperator {
+    public static void main(String args[])
+  {
+ //AND OPERATOR
+
+int ab=10;
+int b=5;
+int c=20;
+System.out.println(ab<b&&ab<c);//false
+System.out.println(ab<b&ab<c);//false
+
+}
